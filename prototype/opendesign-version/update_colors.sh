78bee04663c8200fe1a9e43a30746909d4dd9d11
Adrian Luke Labasan <zionexodus7@protonmail.com>
2026-06-22 11:19:28 +0800

    delete opendesign-version

--- prototype/opendesign-version/update_colors.sh
+++
@@ -1,5 +0,0 @@
-for file in *.html; do
-    sed -i 's/--primary: 220 100% 33%; \/\* Batac Blue \*\//--primary: 139 82.4% 35.7%; \/\* Extracted Green \*\//g' "$file"
-    sed -i 's/--secondary: 49 97% 54%; \/\* Batac Gold \*\//--secondary: 220 93% 56%; \/\* Extracted Blue \*\//g' "$file"
-    sed -i 's/--destructive: 359 85% 44%; \/\* Batac Red \*\//--destructive: 0 100% 48.4%; \/\* Extracted Red \*\//g' "$file"
-done
